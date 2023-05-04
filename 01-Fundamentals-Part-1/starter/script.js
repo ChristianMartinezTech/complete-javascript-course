0f90796f2b5b379a6ee7f496864c9a9cde331beb
ChristianMartinezTech <crismartine.e@gmail.com>
2023-05-04 15:23:28 -0500

    Coded 01

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+// This a quick Javascript introduction :)
+
+
+// Javascript Output
+
+/* JavaScript can "display" data in different ways:
+1. Writing into an HTML element, using innerHTML.
+2. Writing into the HTML output using document.write().
+3. Writing into an alert box, using window.alert().
+4. Writing into the browser console, using console.log().*/
+
+document.getElementById('practice1').innerHTML = 'Second Container';
+document.getElementById('practice2').innerHTML = 'Thrid Container';
+document.getElementById('practice3').innerHTML = 'Thrid Container';
+
+
