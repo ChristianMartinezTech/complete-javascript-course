3eb397930dc821ce07ee04facb714142f0eb0e53
ChristianMartinezTech <crismartine.e@gmail.com>
2023-05-09 23:32:13 -0500

    Experimenting with JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -9,8 +9,11 @@
 3. Writing into an alert box, using window.alert().
 4. Writing into the browser console, using console.log().*/
 
-document.getElementById('practice1').innerHTML = 'Second Container';
-document.getElementById('practice2').innerHTML = 'Thrid Container';
-document.getElementById('practice3').innerHTML = 'Thrid Container';
+console.log("This is going to be shown in the Website's console")
+console.log("not into the website as html data")
 
+let practice1 = document.getElementById('practice1').innerHTML = 'Second Container';
+let practice2 = document.getElementById('practice2').innerHTML = 'Thrid Container';
+let practice3 = document.getElementById('practice3').innerHTML = 'Thrid Container';
 
+practice1.onclick(practice1 = 'You just clicked the second Container'); 
